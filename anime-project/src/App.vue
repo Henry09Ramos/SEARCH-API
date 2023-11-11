8a1825dc6f2c1f590cb732eb4b3499d835ba4eaf
emersonHP <HP@DESKTOP-4FK04Q7>
2023-11-10 19:45:20 -0600

    Paginacion y disenio

--- anime-project/src/App.vue
+++ anime-project/src/App.vue
@@ -1,30 +1,84 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <br>
+  <div class="flex items-center justify-center ">
+    <div class="flex border-2 border-gray-200 rounded">
+        <input type="text" class="px-4 py-2 w-80" placeholder="Search...">
+        <button class="px-4 text-white bg-blue-400 border-l ">
+            Search
+        </button>
+    </div>
+</div>
+<br>
+  <div class="w-full">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div v-for="anime in animes" :key="anime.mal_id" class="w-72 border h-80 rounded-lg">
+                <div class="relative">
+                    <img
+                    :src="anime.images.jpg.large_image_url"
+                    class="w-full h-80 rounded-lg"
+                    alt=""
+                    srcset=""
+                    />
+                    <div class="absolute bottom-0 bg-[rgba(0,0,0,0.7)] w-full flex justify-center items-center rounded-lg p-4">
+                        <p class="text-white text-sm">{{ anime.title }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <PaginationGlobal
+            :pages="pagination.pages"
+            :currentPage="pagination.currentPage"
+            @method="on_change_page"
+            :next="pagination.next_page"
+            :prev="pagination.prev_page"
+            :total-pag="pagination.total"
+        />
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script lang="ts" setup>
+  import axios from "axios";
+import { onMounted, ref } from "vue";
+import { type Data, type GetAnimes } from "./types/images.types";
+import { paginate } from './utils';
+import PaginationGlobal from './components/search.vue';
+
+interface Pagination {
+  pages: Array<string | number>,
+  currentPage: number,
+  total: number,
+  next_page: number,
+  prev_page: number
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+const animes = ref<Data[]>([]);
+
+const pagination = ref<Pagination>({
+  pages: [],
+  currentPage: 1,
+  next_page: 2,
+  prev_page: 0,
+  total: 0
+});
+
+const get_animes = async (page = 1, limit = 6) => {
+  axios.get<GetAnimes>(`https://api.jikan.moe/v4/anime?page=${page}&limit=${limit}`).then(({data}) => {
+    animes.value = data.data;
+    pagination.value = {
+      pages: paginate(data.pagination.current_page,data.pagination.last_visible_page,1),
+      currentPage: data.pagination.current_page,
+      next_page: page === data.pagination.last_visible_page ? 0 : page + 1,
+      prev_page: page === 1 ? 0 : page - 1,
+      total: data.pagination.last_visible_page
+    }
+  });
+};
+
+const on_change_page = (page: number) => {
+  get_animes(page);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+onMounted(async () => {
+ await get_animes();
+})
+</script>
---
+++ anime-project/src/components/search.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    
+  </div>
+  <div>
+
+    <ul class="flex md:hidden mt-5 justify-between">
+      <li>
+        <button @click="changePage(prev)"
+          class="mx-1 bg-blue-500 cursor-pointer rounded-full p-2 w-12 h-12 text-center text-lg text-white"
+          :disabled="currentPage === 1">
+          <i class="fas fa-chevron-left"></i>
+        </button>
+      </li>
+      <li class="flex justify-center items-center">
+        <span>Pagina {{ currentPage }} de {{ totalPag }}</span>
+      </li>
+      <li>
+        <button class="mx-1 bg-blue-500 rounded-full p-2 w-12 h-12 cursor-pointer text-center text-lg text-white"
+          :disabled="currentPage === totalPag" @click="changePage(next)">
+          <i class="fas fa-chevron-right"></i>
+        </button>
+      </li>
+    </ul>
+    <ul class="hidden md:flex mt-6">
+      <li>
+        <button @click="changePage(prev)"
+          class="mx-1 bg-blue-500 cursor-pointer rounded-full p-2 w-12 h-12 text-center text-lg text-white"
+          :disabled="currentPage === 1">
+          <svg xmlns="http://www.w3.org/2000/svg" height="2em"
+            viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+        </button>
+      </li>
+      <li
+        class="mx-1 border font-semibold border-blue rounded-full p-2 w-12 h-12 text-center text-[10px] flex justify-center items-center cursor-pointer"
+        v-for="page in pages" :class="page === currentPage ? 'bg-blue-500 text-white' : ' text-blue-500'"
+        v-bind:key="page" @click="changePage(page)">
+        {{ page }}
+      </li>
+      <li>
+        <button class="mx-1 bg-blue-500 rounded-full p-2 w-12 h-12 cursor-pointer text-center text-lg text-white"
+          :disabled="currentPage === totalPag" @click="changePage(next)">
+          <svg xmlns="http://www.w3.org/2000/svg" class="position:" height="2em"
+            viewBox="-10 60 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg> </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+interface Props {
+  pages: Array<number | string>;
+  currentPage: number;
+  totalPag: number;
+  next: number;
+  prev: number;
+}
+const { pages, currentPage, totalPag, next, prev } = defineProps<Props>();
+
+
+type Emits = {
+  (event: "method", page: number): void;
+
+};
+
+const emits = defineEmits<Emits>();
+
+const changePage = (page: number | string) => {
+  emits("method", Number(page));
+};
+
+
+
+
+
+</script>
